1821e8b65d9e010e5f9ed9084aa3e826b8c2257e
ahmedabougabal <ahmedabougabal@live.com>
2024-07-02 21:32:05 +0300

    added ^ and sqrt functionalities

--- day 4/calc.js
+++ day 4/calc.js
@@ -10,10 +10,20 @@ function key(pressed) {
     displayInp.value = currentInput;
   } else if (value === '=') {
     try {
+      // currentInput = currentInput.replace(/√(\d+(\.\d+)?)/g, 'Math.sqrt($1)');
+      currentInput = currentInput.replace(/\^/g, '**');
       currentInput = eval(currentInput);
       displayInp.value = currentInput;
     } catch (error) {
       displayInp.innerText = "ERROR"
+      currentInput = '';
+    }
+  } else if (value === '√') {
+    try {
+      currentInput = Math.sqrt(currentInput);
+      displayInp.value = currentInput;
+    } catch (error) {
+      displayInp.innerText = "Err"
       currentInput = '';
     }
   } else {
